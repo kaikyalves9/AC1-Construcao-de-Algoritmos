45dfa05a97b2b8210dad9680ad8cd005a8a8c9d5
kaikyalves9 <163069844+kaikyalves9@users.noreply.github.com>
2024-03-27 19:57:38 -0300

    MediaFacens.java

--- Aula5-AC1/MediaFacens.java
+++ Aula5-AC1/MediaFacens.java
@@ -5,23 +5,24 @@ public class MediaFacens {
         // Informa ao usuário sobre o propósito do programa
         System.out.println("Este programa realiza o calcula da média. ");
 
-        // Solicita e recebe o primeiro número do usuário
+        // Solicita e recebe o número da AC1
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a nota da AC1: ");
         double AC1 = scanner.nextDouble();
 
-        // Solicita e recebe o segundo número do usuário
+        // Solicita e recebe o numero da AC2
         System.out.print("Digite o número da AC2: ");
         double AC2 = scanner.nextDouble();
 
-        // Solicitar o terceiro numero
+        // Solicitar o numero da AG
         System.out.print("Digite o número da AG: ");
         double AG = scanner.nextDouble();
 
+        // Solicitar e receber o número da AF
         System.out.print("Digite o número AF: ");
         double AF = scanner.nextDouble();
 
-        // Calcula a soma dos dois números
+        // Calcula a média dos números 
         double resultado = (AC1 * 0.15) + (AC2 * 0.30) + (AG * 0.10) + (AF * 0.45)/1;
 
         // Exibe o resultado para o usuário
